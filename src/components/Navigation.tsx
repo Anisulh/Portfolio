5681091bdf7a28e6cf049b97fa86a056c8d7295e
ubergoose <anisulhaque9391@gmail.com>
2025-02-06 21:22:16 -0500

    migrated to use drop down instead of popover

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -1,6 +1,6 @@
-import { FC, Fragment, RefObject } from "react";
-import { Popover, Transition } from "@headlessui/react";
-import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/outline";
+import { Fragment, RefObject } from "react";
+import { Menu, Transition } from "@headlessui/react";
+import { Bars3Icon } from "@heroicons/react/24/outline";
 
 interface navProps {
   workRef: RefObject<HTMLDivElement> | null;
@@ -28,89 +28,74 @@ export default function Navigation({
   ];
   return (
     <div className="z-10 pb-8 sm:pb-16 md:pb-20 lg:w-full relative">
-      <Popover>
-        <div className="pt-6 ">
-          <nav
-            className="flex items-center justify-between sm:h-10 "
-            aria-label="navigation"
-          >
-            <div className="flex shrink-0 grow items-center lg:grow-0">
-              <div className="flex w-full items-center justify-between md:w-auto">
-                <a
-                  className="cursor-pointer border-2 rounded-lg p-3 text-white border-sky-400 flex items-center hover:bg-white hover:text-sky-500 transition-colors"
-                  onClick={() => homeRef?.current?.scrollIntoView()}
-                >
-                  <span className="sr-only">Anisul Haque's Profolio</span>
-                  AH
-                </a>
-                <div className="-mr-2 flex items-center md:hidden">
-                  <Popover.Button className="inline-flex items-center justify-center rounded-md bg-white p-2 text-gray-400 hover:bg-gray-100 hover:text-gray-500 focus:outline-hidden focus:ring-2 focus:ring-inset focus:ring-sky-500">
-                    <span className="sr-only">Open main menu</span>
-                    <Bars3Icon className="h-6 w-6" aria-hidden="true" />
-                  </Popover.Button>
-                </div>
-              </div>
-            </div>
-            <div className="hidden md:block md:space-x-8 md:pr-4">
-              {navigation.map((item) => (
-                <a
-                  key={item.name}
-                  onClick={() => item.ref?.current?.scrollIntoView()}
-                  className="font-medium text-gray-200 cursor-pointer link link-underline link-underline-black hover:text-gray-400"
-                >
-                  {item.name}
-                </a>
-              ))}
-            </div>
-          </nav>
-        </div>
-
-        <Transition
-          as={Fragment}
-          enter="duration-150 ease-out"
-          enterFrom="opacity-0 scale-95"
-          enterTo="opacity-100 scale-100"
-          leave="duration-100 ease-in"
-          leaveFrom="opacity-100 scale-100"
-          leaveTo="opacity-0 scale-95"
+      <div className="pt-6 ">
+        <nav
+          className="flex items-center justify-between sm:h-10 "
+          aria-label="navigation"
         >
-          <Popover.Panel
-            focus
-            className="absolute inset-x-0 top-0 z-10 origin-top-right transform p-2 transition md:hidden"
-          >
-            <div className="overflow-hidden rounded-lg bg-gray-700 shadow-md ring-1 ring-black ring-opacity-5">
-              <div className="flex items-center justify-between px-5 pt-4">
-                <div>
-                  <a
-                    className="cursor-pointer border-2 rounded-lg p-3 text-white border-sky-400 flex items-center hover:bg-white hover:text-sky-500 transition-colors"
-                    onClick={() => homeRef?.current?.scrollIntoView()}
+          <div className="flex shrink-0 grow items-center lg:grow-0">
+            <div className="flex w-full items-center justify-between md:w-auto">
+              <a
+                className="cursor-pointer border-2 rounded-lg p-3 text-white border-sky-400 flex items-center hover:bg-white hover:text-sky-500 transition-colors"
+                onClick={() => homeRef?.current?.scrollIntoView()}
+              >
+                <span className="sr-only">Anisul Haque's Profolio</span>
+                AH
+              </a>
+              <div className="-mr-2 flex items-center md:hidden">
+                <Menu as="div" className="relative inline-block text-left">
+                  <div>
+                    <Menu.Button className="inline-flex items-center justify-center rounded-md bg-white p-2 text-gray-400 hover:bg-gray-100 hover:text-gray-500 focus:outline-hidden focus:ring-2 focus:ring-inset focus:ring-sky-500">
+                      <span className="sr-only">Open main menu</span>
+                      <Bars3Icon className="h-6 w-6" aria-hidden="true" />
+                    </Menu.Button>
+                  </div>
+                  <Transition
+                    as={Fragment}
+                    enter="transition ease-out duration-100"
+                    enterFrom="transform opacity-0 scale-95"
+                    enterTo="transform opacity-100 scale-100"
+                    leave="transition ease-in duration-75"
+                    leaveFrom="transform opacity-100 scale-100"
+                    leaveTo="transform opacity-0 scale-95"
                   >
-                    <span className="sr-only">Anisul Haque's Profolio</span>
-                    AH
-                  </a>
-                </div>
-                <div className="-mr-2">
-                  <Popover.Button className="inline-flex items-center justify-center rounded-md bg-white p-2 text-gray-400 hover:bg-gray-100 hover:text-gray-500 focus:outline-hidden focus:ring-2 focus:ring-inset focus:ring-sky-500">
-                    <span className="sr-only">Close main menu</span>
-                    <XMarkIcon className="h-6 w-6" aria-hidden="true" />
-                  </Popover.Button>
-                </div>
-              </div>
-              <div className="space-y-1 px-2 pt-2 pb-3">
-                {navigation.map((item) => (
-                  <a
-                    key={item.name}
-                    onClick={() => item.ref?.current?.scrollIntoView()}
-                    className="block link link-underline link-underline-black cursor-pointer rounded-md px-3 py-2 text-base  text-white hover:text-sky-400"
-                  >
-                    {item.name}
-                  </a>
-                ))}
+                    <Menu.Items className="absolute right-0 mt-2 w-56 origin-top-right rounded-md bg-gray-700 shadow-lg ring-1 ring-black/5 focus:outline-none">
+                      {navigation.map((item) => (
+                        <div className="px-1 py-1 ">
+                          <Menu.Item>
+                            {({ active }) => (
+                              <button
+                                key={item.name}
+                                onClick={() =>
+                                  item.ref?.current?.scrollIntoView()
+                                }
+                                className={`block link link-underline link-underline-black cursor-pointer rounded-md px-3 py-2 text-base  text-white transition-colors focus:text-sky-400 ${active && "text-sky-400"}`}
+                              >
+                                {item.name}
+                              </button>
+                            )}
+                          </Menu.Item>
+                        </div>
+                      ))}
+                    </Menu.Items>
+                  </Transition>
+                </Menu>
               </div>
             </div>
-          </Popover.Panel>
-        </Transition>
-      </Popover>
+          </div>
+          <div className="hidden md:block md:space-x-8 md:pr-4">
+            {navigation.map((item) => (
+              <a
+                key={item.name}
+                onClick={() => item.ref?.current?.scrollIntoView()}
+                className="font-medium text-gray-200 cursor-pointer link link-underline link-underline-black hover:text-gray-400"
+              >
+                {item.name}
+              </a>
+            ))}
+          </div>
+        </nav>
+      </div>
     </div>
   );
 }
