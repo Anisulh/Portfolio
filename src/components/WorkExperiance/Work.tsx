eff820a91dc3ea441cf82163873d47b5fb44fce8
ubergoose <anisulhaque9391@gmail.com>
2025-02-08 21:53:47 -0500

    refactor Work component to use ExperienceTimeline for rendering work experiences on larger devices and fix swiping state

--- src/components/WorkExperiance/Work.tsx
+++ src/components/WorkExperiance/Work.tsx
@@ -4,6 +4,7 @@ import WorkExperienceCard from "./WorkExperienceCard";
 import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
+import ExperienceTimeline from "./ExperienceTimeline";
 
 const work = [
   {
@@ -86,104 +87,42 @@ function Work({ workRef }: { workRef: RefObject<HTMLDivElement> | null }) {
       <div ref={workRef} className="text-2xl md:text-4xl mb-10 font-bold ">
         Work Experience
       </div>
-      <div className="flex flex-col md:flex-row md:gap-10 pl-2 md:pl-10">
-        <ol className="relative border-s border-gray-500">
+      <div className="flex flex-col md:flex-row md:gap-10 pl-2 md:pl-10 md:pr-40 w-full h-full">
+        <ol className="relative border-s border-gray-500 md:w-1/2">
           {work.map((val, idx) => (
-            <li
-              className={`mb-10 ms-4 p-2 transition-colors ${selectedCard === idx && " bg-gray-700 rounded-md"}`}
-            >
-              <div className="absolute w-3 h-3  rounded-full mt-1.5 -start-1.5 border border-white dark:border-gray-900 bg-sky-500"></div>
-              <time className="mb-1 text-sm font-normal leading-none text-gray-400">
-                {val.date}
-              </time>
-              <div className="flex gap-2 items-center">
-                <h3 className="md:text-lg font-semibold text-white">
-                  {val.name}
-                </h3>
-                {idx === 0 && (
-                  <span className="bg-sky-100 text-sky-700 text-sm font-medium me-2 px-2.5 py-0.5 rounded-sm  ms-3">
-                    Present
-                  </span>
-                )}
-              </div>
-              <p className="mb-4 text-sm md:text-base font-normal text-gray-200">
-                {val.title}
-              </p>
-              <button
-                className="hidden md:inline-flex items-center px-4 py-2 text-sm font-medium text-gray-200 bg-gray-800 border border-gray-600 rounded-lg "
-                onClick={() => {
-                  setSelectedCard(idx);
-                }}
-              >
-                Learn more
-                <svg
-                  className="w-3 h-3 ms-2 rtl:rotate-180"
-                  aria-hidden="true"
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
-                  viewBox="0 0 14 10"
-                >
-                  <path
-                    stroke="currentColor"
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                    stroke-width="2"
-                    d="M1 5h12m0 0L9 1m4 4L9 9"
-                  />
-                </svg>
-              </button>
-              <button
-                className="md:hidden inline-flex items-center px-4 py-2 text-sm font-medium text-gray-200 bg-gray-800 border border-gray-600 rounded-lg "
-                onClick={() => {
-                  setSelectedCard(idx);
-                  sliderRef.current?.slickGoTo(idx);
-                  workCardRef.current?.scrollIntoView({ behavior: "smooth" });
-                }}
-              >
-                Learn more
-                <svg
-                  className="w-3 h-3 ms-2 rtl:rotate-180"
-                  aria-hidden="true"
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
-                  viewBox="0 0 14 10"
-                >
-                  <path
-                    stroke="currentColor"
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                    stroke-width="2"
-                    d="M1 5h12m0 0L9 1m4 4L9 9"
-                  />
-                </svg>
-              </button>
-            </li>
+            <ExperienceTimeline
+              key={idx}
+              val={val}
+              idx={idx}
+              selectedCard={selectedCard}
+              setSelectedCard={setSelectedCard}
+              sliderRef={sliderRef}
+              workCardRef={workCardRef}
+            />
           ))}
         </ol>
-        <div ref={workCardRef} className="pt-16 md:pt-0">
+        <div ref={workCardRef} className="pt-16 md:pt-0 md:w-[85%] h-full">
           <Slider
             {...settings}
             ref={sliderRef}
             onSwipe={(swipeDirection) => {
               if (swipeDirection === "left") {
-                setSelectedCard(selectedCard + 1);
+                setSelectedCard((selectedCard + 1) % work.length);
               } else if (swipeDirection === "right") {
-                setSelectedCard(selectedCard - 1);
+                setSelectedCard((selectedCard - 1 + work.length) % work.length);
               }
             }}
           >
-            {work.map((experience, index) => {
-              return (
-                <m.div
-                  key={index}
-                  initial={{ opacity: 0 }}
-                  whileInView={{ opacity: 1 }}
-                  className="mb-14 p-4 h-full"
-                >
-                  <WorkExperienceCard experience={experience} />
-                </m.div>
-              );
-            })}
+            {work.map((experience, index) => (
+              <m.div
+                key={index}
+                initial={{ opacity: 0 }}
+                whileInView={{ opacity: 1 }}
+                className="mb-14 p-4 h-full"
+              >
+                <WorkExperienceCard experience={experience} />
+              </m.div>
+            ))}
           </Slider>
         </div>
       </div>
